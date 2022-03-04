3a89b4b027241ab249e9c4a30db8390d96402340
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-03-04 17:57:20 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Dara To on 2022-03-04.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
