6c56a5ec124656d934a686f92b7476974688c603
Dara-To <33760969+Dara-To@users.noreply.github.com>
2022-03-18 13:50:33 -0400

    Part 6

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -16,6 +19,19 @@ struct ContentView: View {
                     Text("Overview")
                         .font(.title2)
                         .bold()
+                    
+                    // MARK: Chart
+                    CardView {
+                        VStack {
+                            ChartLabel("$900", type: .title)
+                            
+                            LineChart()
+                        }
+                        .background(Color.systemBackground)
+                    }
+                    .data(demoData)
+                    .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+                    .frame(height: 300)
                     
                     // MARK: Transaction List
                     RecentTransactionList()
